55047d23ee46d8a39ca92f64ead9de373813c013
EgleJu <juzokaite.egle@gmail.com>
2019-07-03 17:02:31 +0300

    added ClearDB

--- comment_section.php
+++ comment_section.php
@@ -1,7 +1,13 @@
 <?php
 
-$connect = new PDO('mysql:host=@us-cdbr-iron-east-02.cleardb.net;
-dbname:/heroku_8a8fbf4f3d60ae4', 'b0a1c84d43e334', '44a5a5d7');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["us-cdbr-iron-east-02.cleardb.net"];
+$username = $url["b0a1c84d43e334"];
+$password = $url["44a5a5d7"];
+$db = substr($url["heroku_8a8fbf4f3d60ae4"], 1);
+
+$connect = new mysqli($server, $username, $password, $db);
 
 $error = '';
 $comment_name = '';
